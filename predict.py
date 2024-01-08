8e39c4a79d5dc702c0997ce2cf461b61d7cd8445
gaurav kumar <gauravkcs50@gmail.com>
2024-01-09 01:40:20 +0530

    fix predict.py

--- predict.py
+++ predict.py
@@ -15,8 +15,8 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-checkpoint_dir = "./checkpoints"
-result_dir = "./results"
+checkpoint_dir = "checkpoints"
+result_dir = "results"
 
 class Predictor(BasePredictor):
     def setup(self):
@@ -24,25 +24,21 @@ class Predictor(BasePredictor):
         print("Setup Predictions...")
         device = "cuda"
 
-        current_code_path = sys.argv[0]
-        current_root_path = os.path.split(current_code_path)[0]
-        os.environ['TORCH_HOME'] = os.path.join(current_root_path, checkpoint_dir)
+        path_of_lm_croper = os.path.join(checkpoint_dir, 'shape_predictor_68_face_landmarks.dat')
+        path_of_net_recon_model = os.path.join(checkpoint_dir, 'epoch_20.pth')
+        dir_of_BFM_fitting = os.path.join(checkpoint_dir, 'BFM_Fitting')
+        wav2lip_checkpoint = os.path.join(checkpoint_dir, 'wav2lip.pth')
 
-        path_of_lm_croper = os.path.join(current_root_path, checkpoint_dir, 'shape_predictor_68_face_landmarks.dat')
-        path_of_net_recon_model = os.path.join(current_root_path, checkpoint_dir, 'epoch_20.pth')
-        dir_of_BFM_fitting = os.path.join(current_root_path, checkpoint_dir, 'BFM_Fitting')
-        wav2lip_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'wav2lip.pth')
+        audio2pose_checkpoint = os.path.join(checkpoint_dir, 'auido2pose_00140-model.pth')
+        audio2pose_yaml_path = os.path.join('src', 'config', 'auido2pose.yaml')
 
-        audio2pose_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'auido2pose_00140-model.pth')
-        audio2pose_yaml_path = os.path.join(current_root_path, 'src', 'config', 'auido2pose.yaml')
+        audio2exp_checkpoint = os.path.join(checkpoint_dir, 'auido2exp_00300-model.pth')
+        audio2exp_yaml_path = os.path.join('src', 'config', 'auido2exp.yaml')
 
-        audio2exp_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'auido2exp_00300-model.pth')
-        audio2exp_yaml_path = os.path.join(current_root_path, 'src', 'config', 'auido2exp.yaml')
+        free_view_checkpoint = os.path.join(checkpoint_dir, 'facevid2vid_00189-model.pth.tar')
 
-        free_view_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'facevid2vid_00189-model.pth.tar')
-
-        mapping_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'mapping_00109-model.pth.tar')
-        facerender_yaml_path = os.path.join(current_root_path, 'src', 'config', 'facerender_still.yaml')
+        mapping_checkpoint = os.path.join(checkpoint_dir, 'mapping_00109-model.pth.tar')
+        facerender_yaml_path = os.path.join('src', 'config', 'facerender_still.yaml')
 
         # init model
         print("Loading Model Weights...")
@@ -61,10 +57,10 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        vid_path: Path = Input(
+        video_input_path: Path = Input(
             description="Upload the source video usually a .mp4 file",
         ),
-        audio_path: Path = Input(
+        audio_input_path: Path = Input(
             description="Upload the driven audio, accepts .wav and .mp4 file",
         ),
         enhancer_region: str = Input(
@@ -80,7 +76,8 @@ class Predictor(BasePredictor):
         """Run a single prediction on the model"""
         print("Start Predictions...")
         device = "cuda"
-        
+        video_path = str(video_input_path)
+        audio_path = str(audio_input_path)
         # set basic peridictions params
         save_dir = os.path.join(result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
         os.makedirs(save_dir, exist_ok=True)
@@ -90,7 +87,7 @@ class Predictor(BasePredictor):
         first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
         os.makedirs(first_frame_dir, exist_ok=True)
         print('3DMM Extraction for source image')
-        first_coeff_path, crop_pic_path, crop_info = self.preprocess_model.generate(vid_path, first_frame_dir)
+        first_coeff_path, crop_pic_path, crop_info = self.preprocess_model.generate(video_path, first_frame_dir)
         if first_coeff_path is None:
             print("Can't get the coeffs of the input")
             return
@@ -99,7 +96,7 @@ class Predictor(BasePredictor):
         coeff_path = self.audio_to_coeff.generate(batch, save_dir)
         # coeff2video
         data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, device)
-        tmp_path, new_audio_path, return_path = self.animate_from_coeff.generate(data, save_dir, vid_path, crop_info, self.restorer_model, self.enhancer_model, enhancer_region)
+        tmp_path, new_audio_path, return_path = self.animate_from_coeff.generate(data, save_dir, video_path, crop_info, self.restorer_model, self.enhancer_model, enhancer_region)
         final_mp4_path = return_path 
         
         torch.cuda.empty_cache()
